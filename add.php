e5da8771658d78dd6d796a5d0a6f992885fec57a
Sakura Tanaka <sito6496t@gmail.com>
2024-02-16 18:53:26 +0100

    Add query to fetch data

--- add.php
+++ add.php
@@ -1,7 +1,46 @@
 <?php
 
-   if(isset($_GET['submit'])){
-    echo $_GET['email'];
+   $errors=array('email'=>'','title'=>'','ingredients'=>'');
+   $email=   $title=   $ingredients="";
+  
+
+   if(isset($_POST['submit'])){
+    if(empty($_POST['email'])){
+        $errors['email']= 'An email is required <br/>';
+        }else{
+            $email=($_POST['email']);
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL))
+        {
+           $errors['email']= "email must be a valid address";
+        }
+    }
+
+    if(empty($_POST['title'])){
+        $errors['title']='A title is required <br/>';
+    }else{
+        $title=$_POST['title'];
+        if(!preg_match('/^[a-zA-Z\s]+$/', $title))
+        {
+            $errors['title']=  "Title must be letters and spaces only";
+        }
+    }
+
+    if(empty($_POST['ingredients'])){
+        $errors['ingredients']=  'A title is required <br/>';
+    }else{
+         $ingredients=$_POST['ingredients'];
+        if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/',$_POST['ingredients']))
+        {
+            $errors['ingredients']= "Ingtredients must be a comma separeted list";
+        }
+    }
+
+    if(array_filter($errors)){
+        echo 'errors in the form';
+    } else {
+        echo 'form is valid';
+        header('Location:index.php');
+    }
    }
 ?>
 
@@ -10,13 +49,16 @@
     <?php include ('./template/header.php');?>
 <section class="container grey-text">
     <h4 class="center">Add a Pizza</h4>
-    <form action="add.php" method="GET" class="white">
+    <form action="add.php" method="POST" class="white">
         <label for="">Your Email:</label>
-        <input type="text" name="email">
+        <input type="text" name="email" value='<?=htmlspecialchars($email)?>'>
+        <div class="red-text"><?=$errors['email']?></div>
         <label for="">Pizza Title:</label>
-        <input type="text" name="title">
+        <input type="text" name="title" value='<?=htmlspecialchars($title)?>'>
+        <div class="red-text"><?=$errors['title']?></div>
         <label for="">Ingredients (comma separated):</label>
-        <input type="text" name="ingredients">
+        <input type="text" name="ingredients" value='<?=htmlspecialchars($ingredients)?>'>
+        <div class="red-text"><?=$errors['ingredients']?></div>
      <div class="center">
         <input type="submit" name="submit" value="submit" class="btn brand z-depth-0"/>
      </div>
--- index.php
+++ index.php
@@ -1,11 +1,44 @@
 <?php
+$conn=mysqli_connect('localhost','san','test123','ninja pizza');
 
+if(!$conn)
+{
+ echo 'Connection error'.mysqli_connect_error();
+}
+
+//query for all pizzas
+
+$sql='SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
+$result=mysqli_query($conn,$sql);
+$pizzas=mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+//claen up
+mysqli_free_result($result);
+mysqli_close($conn);
+print_r($pizzas);
 ?>
 
 <!DOCTYPE html>
 <html>
     <?php include ('./template/header.php');?>
 
+
+    <h4 class="center grey-text">Pizzas!</h4>
+    <div class="container">
+        <div class="row">
+            <?php foreach($pizzas as $pizza){?>
+                <div class='col s6 md3'>
+                    <div class="card z-depth-0">
+                        <h6><?=htmlspecialchars($pizza['title'])?></h6>
+                        <div><?=htmlspecialchars($pizza['ingredients'])?></div>
+                    </div>
+                    <div class="card-action right-align">
+                        <a href="#" class="brand-text">more info</a>
+                    </div>
+                </div>
+            <?php }?>
+        </div>
+    </div>
 
     <?php include ('./template/footer.php');?>
 </html>
