d0b658948cbec3d8aba54280f591b4c9e41353d4
Sakura Tanaka <sito6496t@gmail.com>
2024-02-18 10:12:36 +0100

    finish course

--- details.php
+++ details.php
@@ -1,5 +1,16 @@
 <?php
 include ('config/db_connect.php');
+
+if(isset($_POST['delete'])){
+    $id_to_delete=mysqli_real_escape_string($conn,$_POST['id_to_delete']);
+
+    $sql="DELETE FROM pizzas WHERE id=$id_to_delete";
+    if(mysqli_query($conn,$sql)){
+        header('Location: index.php');
+    }else{
+        echo 'query error'.mysqli_error($conn);
+    }
+}
 //check get req
 if(isset($_GET['id'])){
    $id=mysqli_real_escape_string($conn, $_GET['id']);
@@ -20,14 +31,14 @@ if(isset($_GET['id'])){
 <!DOCTYPE html>
 <html lang="en">
 <?php include ('./template/header.php');?>
-<div class="container center">
+<div class="container center grey-text">
     <?php if($pizza):?>
         <h4><?=htmlspecialchars($pizza['title'])?></h4>
         <p>Created by : <?=htmlspecialchars($pizza['email'])?></p>
         <p><?=date($pizza['created_at'])?></p>
         <h5>Ingredients: </h5>
         <p><?=htmlspecialchars($pizza['ingredients'])?></p>
-        <form>
+        <form action="details.php" method="POST">
             <input type='hidden' name="id_to_delete" value="<?=$pizza['id']?>"/> 
             <input type="submit" name="delete" value="Delete" class="btn brand z-depth-0"/>
         </form>
